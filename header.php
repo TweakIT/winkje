f01dcb15c008ac800269e2cff61d6354bf9b30f1
Koen Schellens <k.schellens@gmail.com>
2019-05-22 16:46:35 +0200

    added blinking and gazing basic pages

---
+++ header.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+
+    <?php 
+    // Set base URL for CSS and JS paths 
+    $baseURL = "http://" . $_SERVER['SERVER_NAME']; 
+    ?>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
+        <link rel="stylesheet" href="<?php echo $baseURL;?>/css/paper_bootstrap.css">
+        <link rel="stylesheet" href="<?php echo $baseURL;?>/css/winkje_custom_css.css">
+        <link rel="stylesheet" href="<?php echo $baseURL;?>/css/font-awesome.css">
+ 
+
+        <script src="<?php echo $baseURL;?>/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    
+  
+    
+    <body>
+        
+
+    <div class="container">
+        <div class="row">
--- index.php
+++ index.php
@@ -1,38 +1,4 @@
-<!doctype html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
-    <head>
-
-    <?php 
-    // Set base URL for CSS and JS paths 
-    $baseURL = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; 
-    ?>
-
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
-
-        <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
-        <link rel="stylesheet" href="<?php echo $baseURL;?>css/paper_bootstrap.css">
-        <link rel="stylesheet" href="<?php echo $baseURL;?>css/winkje_custom_css.css">
-        <link rel="stylesheet" href="<?php echo $baseURL;?>css/font-awesome.css">
- 
-
-        <script src="<?php echo $baseURL;?>js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
-    </head>
-    
-  
-    
-    <body>
-        
-
-    <div class="container">
-        <div class="row">
+<?php include('header.php'); ?>
             <div class="col-md-12">
                 <div id="mainJumbo" class="jumbotron main_jumbo">
                     <h3 class="text-center">Mobile Interaction experiment <i class="icon-eye-open"> </i></h3>
@@ -61,37 +27,9 @@
                         
                         
                         
-                    </div>
-                </div>
-            </div>
-        </div>
-            
+                    </div>  <!-- Jumbo end   -->
+                </div>  <!-- Col-md-12 end -->
 
+            <?php include('footer.php'); ?>
 
-
-
-
-   
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
-
-        <script src="js/vendor/bootstrap.min.js"></script>
-
-        <script src="js/main.js"></script>
-
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-        </script>
-
-        <script>
-            
-       
-       </script>
-    </body>
-</html>
+           
